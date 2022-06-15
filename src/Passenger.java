a145798a8e1bb88ddd7b693d6bd6035e1f77f60e
Quyen Huynh <quyenh678@gmail.com>
2022-06-15 12:57:02 -0400

    created passenger class

---
+++ src/Passenger.java
@@ -0,0 +1,43 @@
+public class Passenger {
+
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String gender;
+    private int age;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public String getGender() {
+        return this.gender;
+    }
+}
